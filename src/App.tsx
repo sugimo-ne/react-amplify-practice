94a10faa83b300024f735200fe651f0d7eda2d55
sugimo-ne <kein.sugimoto@gmail.com>
2020-12-23 00:36:02 +0900

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Amplify from '@aws-amplify/core';
+import awsmobile from './aws-exports';
+import { withAuthenticator } from 'aws-amplify-react';
+import HomeView from "./pages/HomeView"
+
+Amplify.configure(awsmobile);
+export const App: React.FunctionComponent=() => {
+  return(
+    <Router>
+      <Switch>
+        <Route exact path="/" component={HomeView}></Route>
+      </Switch>
+    </Router>
+  )
 }
 
-export default App;
+//@ts-ignore
+export default withAuthenticator(App , {
+  signUpConfig:{
+    hiddenDefaults: ['phone_number']
+  }
+})
